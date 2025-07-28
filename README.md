2fb41ce4ba192ed4dd16e12cf51c84260bb83cd4
Baboyma Kagniniwa <baboyma@gmail.com>
2025-07-28 13:57:05 -0400

    DRAFT - Pause catcher work

--- README.md
+++ README.md
@@ -17,6 +17,8 @@ GAME NIGHTS - Playful activities written by ground-ups and for kids
 
 `poetry run python -m frolics -g snake`
 
+`poetry run python snake.py`
+
 `-g` can be replaced with `--game` and is used to pass the name of the game to be executed.
 
 ## Have ideas?
--- frolics/catcher.py
+++ frolics/catcher.py
@@ -58,12 +58,12 @@ class Catcher(tk.Tk):
         #self.color = choice([shp.values() for shp in self.shapes])
 
         # Snake positions - initial positions should not change after each session
-        self.snake_init_positions = [(100, 100), (80, 100), (60, 100)]
-        self.snake_positions = copy.deepcopy(self.snake_init_positions)
+        self.shapes_init_positions = [(100, 100), (80, 100), (60, 100)]
+        self.shapes_positions = copy.deepcopy(self.shapes_init_positions)
 
         # Food positions - Retain initial food position after each sessin
-        self.food_init_position = (200, 200)
-        self.food_position = copy.deepcopy(self.food_init_position)
+        self.catcher_init_position = (200, 200)
+        self.catcher_position = copy.deepcopy(self.catcher_init_position)
 
         # Initiate Application
         self.initiate()
@@ -128,8 +128,48 @@ class Catcher(tk.Tk):
             outline="#525d69"
         )
 
+        # Draw the catcher line at the bottom of the canvas
+        self.draw_catcher()
+
+        # Draw shapes at initial positions
+        self.draw_shapes()
+
     def build_gui(self):
         pass
+
+    def draw_catcher(self):
+        self.canvas.create_line(
+            self._play_zone['x0'] + (self.pad * 2),
+            self._play_zone['y1'] - (self.pad * 5),
+            self._play_zone['x1'] - (self.pad * 2),
+            self._play_zone['y1'] - (self.pad * 5),
+            width = 20,
+            fill = "#FFF"
+        )
+
+    def draw_(self):
+        self.canvas.create_line(
+            self._play_zone['x0'] + (self.pad * 2),
+            self._play_zone['y1'] - (self.pad * 5),
+            self._play_zone['x1'] - (self.pad * 2),
+            self._play_zone['y1'] - (self.pad * 5),
+            width = 20,
+            fill = "#FFF"
+        )
+
+    def draw_shapes(self):
+        r = 100
+        shp_y = self._play_zone['y0'] + r + (r/2)
+        shp_x = (self._play_zone['x0'] - self._play_zone['x0']) / len(self.shapes)
+
+        self.canvas.create_oval(
+            self._play_zone['x0'] + (self.pad * 3),
+            self._play_zone['y0'] + (self.pad * 3),
+            self._play_zone['x0'] + (self.pad * 10),
+            self._play_zone['y0'] + (self.pad * 10),
+            width = 2,
+            fill = "#FFF"
+        )
 
 def check_bin_catch(x, y):
     red_bin_x1, red_bin_x2, red_bin_y1, red_bin_y2 = 100
